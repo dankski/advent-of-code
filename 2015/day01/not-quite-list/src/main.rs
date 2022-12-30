359dd9d8fd1384fa70550333021c72b384209501
Giacomo Amoroso <giacomo@amoroso.me>
2022-12-30 11:14:44 +0100

    AOC 2015 Day 01: Solved fist part

--- 2015/day01/not-quite-list/src/main.rs
+++ 2015/day01/not-quite-list/src/main.rs
@@ -14,7 +14,9 @@ fn evaluate_floor(input: &Vec<char>) -> i32 {
     for c in input {
         if *c == '(' {
             floor = floor + 1;
-        } else {
+        } 
+
+        if *c == ')' {
             floor = floor - 1;
         }
     }
@@ -22,10 +24,14 @@ fn evaluate_floor(input: &Vec<char>) -> i32 {
     floor
 }
 
+fn convert_input_vec (input: &String) -> Vec<char> {
+    input.chars().collect()
+}
 
 
 fn main() {
-    println!("Hello, world!");
+    let puzzle_input = std::fs::read_to_string("assets/input.txt").expect("Should contain the puzzle input.");
+    println!("Puzzle One Answer is 280, and result is {}", evaluate_floor(&convert_input_vec(&puzzle_input)));
 }
 
 #[cfg(test)]
@@ -35,7 +41,7 @@ mod tests {
     #[test]
     fn should_evaluate_floor_0 () {
         let v1 = vec!['(','(',')',')'];
-        let v2 = vec!['(',')','(',')'];
+        let v2 = vec![')','(','(',')'];
 
         assert_eq!(0, evaluate_floor(&v1));
         assert_eq!(0, evaluate_floor(&v2));
@@ -46,9 +52,38 @@ mod tests {
     fn should_evaluate_floor_3 () {
         let v1 = vec!['(','(','('];
         let v2 = vec!['(','(',')','(','(',')','('];
+        let v3 = vec![')',')','(','(','(','(','('];
 
         assert_eq!(3, evaluate_floor(&v1));
         assert_eq!(3, evaluate_floor(&v2));
+        assert_eq!(3, evaluate_floor(&v3));
+    }
+
+    #[test]
+    fn should_evaluate_to_minus_one () {
+
+        let v1 = vec!['(',')',')'];
+        let v2 = vec![')',')','('];
+
+        assert_eq!(-1, evaluate_floor(&v1));
+        assert_eq!(-1, evaluate_floor(&v2));
+    }
+
+    #[test]
+    fn should_evaluate_to_minus_three () {
+
+        let v1 = vec![')',')',')'];
+        let v2 = vec![')','(',')',')','(',')',')'];
+
+        assert_eq!(-3, evaluate_floor(&v1));
+        assert_eq!(-3, evaluate_floor(&v2));
+    }
+
+    #[test]
+    fn should_convert_vec () {
+        let input = String::from("(())");
+        let v: Vec<char> = convert_input_vec(&input);
+        
+        assert_eq!(4, v.len());
     }
 }
-
