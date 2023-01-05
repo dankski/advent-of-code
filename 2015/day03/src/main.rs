a8b48f2dd3cf42b81399e01a483121b1eba3fd94
Giacomo Amoroso <giacomo@amoroso.me>
2023-01-05 19:17:59 +0100

    Starting Day 03

---
+++ 2015/day03/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let puzzle_input = std::fs::read_to_string("assets/puzzle-input.txt").expect("Should contain the puzzle input.");
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+}
