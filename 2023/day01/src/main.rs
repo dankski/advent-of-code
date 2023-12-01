222f045aff1b3d6ccc6c8674551f69ac1cca05d9
giacomo <giacomo@amoroso.me>
2023-12-01 22:15:15 +0100

    First golden star

--- 2023/day01/src/main.rs
+++ 2023/day01/src/main.rs
@@ -3,7 +3,13 @@ mod util;
 fn main() {
 
     let puzzle_input = std::fs::read_to_string("assets/input.txt").expect("Should contain the puzzle input.");
-    let dims = input.split('\n').collect::<Vec<&str>>();
-    let v = util::calibration_value(&"1abc21abc2".to_string());
-    println!("[SOLUTION] {v}");
+    let calibrations = puzzle_input.split('\n').collect::<Vec<&str>>();
+
+    let mut sum: u32 = 0;
+    for calibration_value in calibrations {
+        let v = util::calibration_value(&calibration_value.to_string());
+        sum = sum + v;
+    }
+   
+    println!("\n[SOLUTION] {sum}\n");
 }
