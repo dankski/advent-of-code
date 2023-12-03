3d59dbdc6e1fac482e3b73d536289ac49b724305
Giacomo Amoroso <giacomo@amoroso.me>
2023-12-03 14:24:47 +0100

    WIP D2P2

--- 2023/day02/src/config/game.rs
+++ 2023/day02/src/config/game.rs
@@ -21,14 +21,18 @@ impl Game {
         let values: Vec<&str> = g.split(|c| c == ':' || c == ';').collect();
         
         let gid = self.extract_game_id(values[0]);
+        let mut min_set:(u32,u32,u32) = (0,0,0);
         for i in 1..values.len() {
             let r = values[i];
-            if self.valdiate_round(r) != true {
+            let validation_result = self.valdiate_round(r, min_set);
+            if validation_result.0 != true {
                 return (0, 0);
             }
+            min_set = validation_result.1;
         }
 
-        return (gid, 0);
+        println!("\n min set: rgb({}, {}, {}) \n", min_set.0, min_set.1, min_set.2);
+        return (gid, min_set.0 * min_set.1 * min_set.2);
     }
 
     fn extract_game_id(self, s: &str) -> u32 {
@@ -36,13 +40,14 @@ impl Game {
         return v[1].parse::<u32>().unwrap();
     }
 
-    fn valdiate_round(self, round: &str) -> bool {
+    fn valdiate_round(self, round: &str, min_set: (u32, u32, u32)) -> (bool, (u32, u32, u32)) {
         let colors: Vec<String> = round.split(',').map(|e| e.to_string()).collect();
  
         let mut red = 0;
-        let mut blue = 0;
         let mut green = 0;
+        let mut blue = 0;
 
+        let mut min_set_result = min_set;
         for color in colors {
 
             if color.contains("red") {
@@ -60,26 +65,41 @@ impl Game {
                 green = result[0].parse::<u32>().unwrap();
             }
 
-            if !self.validate((red, blue, green)) {
-                return false;
+            if !self.validate((red, green, blue)) {
+                return (false, (0,0,0));
             }
+
+            min_set_result = self.minimum_cube_set(min_set_result, (red, green, blue));
         }
 
 
-        return true;
+        return (true, min_set_result);
     }
 
     fn validate(self, round: (u32, u32, u32)) -> bool {
 
-        if self.load.red >= round.0 && self.load.blue >= round.1 && self.load.green >= round.2 {
+        if self.load.red >= round.0 && self.load.green >= round.1 && self.load.blue >= round.2 {
             return true;
         }
         return false;
     }
 
-    fn minimum_cube_set(self) -> (u32, u32, u32) {
+    fn minimum_cube_set(self, min_cube_set: (u32, u32, u32), current_set: (u32, u32, u32)) -> (u32, u32, u32) {
+        let mut min_result: (u32, u32, u32) = min_cube_set;
 
-        return (0,0,0);
+        if current_set.0 >= min_cube_set.0 {
+            min_result.0 = current_set.0;
+        }
+
+        if current_set.1 >= min_cube_set.1 {
+            min_result.1 = current_set.1;
+        }
+
+        if current_set.2 >= min_cube_set.2 {
+            min_result.2 = current_set.2;
+        }
+        
+        return min_result;
     }
     
 }
@@ -93,14 +113,14 @@ mod tests {
     fn should_return_game1_ok() {
         let g = Game::new();
         
-        assert_eq!(g.possible_game(&"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string()), (1, 0));
+        assert_eq!(g.possible_game(&"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string()), (1, 48));
     }
 
     #[test]
     fn should_return_game2_ok() {
         let g = Game::new();
 
-        assert_eq!(g.possible_game(&"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue".to_string()), (2, 0));
+        assert_eq!(g.possible_game(&"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue".to_string()), (2, 12));
     }
 
     #[test]
