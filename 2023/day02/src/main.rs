cd2b85f984d43ff0869339912606b7732399ce07
Giacomo Amoroso <giacomo@amoroso.me>
2023-12-03 12:18:37 +0100

    Working Party 1

--- 2023/day02/src/main.rs
+++ 2023/day02/src/main.rs
@@ -5,5 +5,13 @@ use config::Game;
 fn main() {
     let game = Game::new();
 
-    println!("Hello, world!");
+    let puzzle_input = std::fs::read_to_string("assets/input.txt").expect("Should contain the puzzle input.");
+    let games = puzzle_input.split('\n').collect::<Vec<&str>>();
+
+    let mut sum = 0;
+    for round in games {
+        sum = sum + game.possible_game(&round.to_string());
+    }
+
+    println!("\n[SOLUTION] {sum}\n");
 }
