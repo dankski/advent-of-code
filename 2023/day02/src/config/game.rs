92cfaf27cb26821380fbe07167f89c154f57830d
Giacomo Amoroso <giacomo@amoroso.me>
2023-12-03 12:12:27 +0100

    UT working

--- 2023/day02/src/config/game.rs
+++ 2023/day02/src/config/game.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Copy, Clone)]
 pub struct Game {
     load: BagLoading,
@@ -18,7 +20,7 @@ impl Game {
 
     pub fn possible_game(self, g: &String) -> u32 {
         let values: Vec<&str> = g.split(|c| c == ':' || c == ';').collect();
-        // values.into_iter().for_each(|e| println!("{e}"));
+        
         let gid = self.extract_game_id(values[0]);
         for i in 1..values.len() {
             let r = values[i];
@@ -27,7 +29,7 @@ impl Game {
             }
         }
 
-        return 0;
+        return gid;
     }
 
     fn extract_game_id(self, s: &str) -> u32 {
@@ -36,31 +38,45 @@ impl Game {
     }
 
     fn valdiate_round(self, round: &str) -> bool {
-
         let colors: Vec<String> = round.split(',').map(|e| e.to_string()).collect();
+ 
+        let mut red = 0;
+        let mut blue = 0;
+        let mut green = 0;
 
-        let red = 0;
-        let green = 0;
-        let blue = 0;
+        for color in colors {
 
-        // for color in colors {
-        //     println!("{}", color);
-        //     red: u32 = self.read_color_value(&"red", &color);       
-        //     green = self.read_color_value(&"green", &color);
-        //     let blue = self.read_color_value(&"blue", &color);
-    
-        //     println!("{red} {green} {blue}");  
-        // }
+            if color.contains("red") {
+                let result: Vec<&str> = color.trim().split(char::is_whitespace).collect();
+                red = result[0].parse::<u32>().unwrap();
+            }
+
+            if color.contains("blue") {
+                let result: Vec<&str> = color.trim().split(char::is_whitespace).collect();
+                blue = result[0].parse::<u32>().unwrap();
+            }
+
+            if color.contains("green") {
+                let result: Vec<&str> = color.trim().split(char::is_whitespace).collect();
+                green = result[0].parse::<u32>().unwrap();
+            }
+
+            if !self.validate((red, blue, green)) {
+                return false;
+            }
+        }
+
+        println!("{red} {blue} {green}");
 
         return true;
     }
 
-    fn read_color_value(self,color: &str, s: &String) -> u32 {
-        if s.contains(color) {
-            let result: Vec<&str> = s.trim().split(char::is_whitespace).collect();
-            return result[0].parse::<u32>().unwrap();
+    fn validate(self, round: (u32, u32, u32)) -> bool {
+
+        if self.load.red >= round.0 && self.load.blue >= round.1 && self.load.green >= round.2 {
+            return true;
         }
-        return 0;
+        return false;
     }
     
 }
@@ -77,6 +93,33 @@ mod tests {
         assert_eq!(g.possible_game(&"Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green".to_string()), 1);
     }
 
+    #[test]
+    fn should_return_game2_ok() {
+        let g = Game::new();
 
+        assert_eq!(g.possible_game(&"Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue".to_string()), 2);
+    }
+
+    #[test]
+    fn should_return_game3_not_ok() {
+        let g = Game::new();
+
+        assert_eq!(g.possible_game(&"Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red".to_string()), 0);
+    }
+
+
+    #[test]
+    fn should_return_game4_not_ok() {
+        let g = Game::new();
+
+        assert_eq!(g.possible_game(&"Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red".to_string()), 0);
+    }
+
+    #[test]
+    fn should_return_game5_ok() {
+        let g = Game::new();
+
+        assert_eq!(g.possible_game(&"Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".to_string()), 5);
+    }
 
 }
