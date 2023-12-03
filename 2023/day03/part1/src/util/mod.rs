b5a63ffe85109966f6d68728e7c0b0503a328938
Giacomo Amoroso <giacomo@amoroso.me>
2023-12-03 20:48:24 +0100

    Working on position algo

--- 2023/day03/part1/src/util/mod.rs
+++ 2023/day03/part1/src/util/mod.rs
@@ -1,29 +1,44 @@
 
 struct Parts {
     map: Vec<Vec<char>>,
+    x_dim: u32,
+    y_dim: u32
 }
 
+impl Parts {
+    fn init(&mut self) {
+        self.x_dim = self.map[0].len() as u32;
+        self.y_dim = self.map.len() as u32;
+    }
+
+    fn posible_positions(self, pos: u32) -> Vec<(u32,u32)> {
+        
+    }
+}
 pub fn parts_sum(schema: &String) -> u32 {
     
     let parts = load_parts_map(schema);
     
-    for part in parts {
-        
-    }
+    for part in parts.map {
+        for c in part.chars() {
+            
+        }
+    }   
     
     return 0;
 }
 
 fn load_parts_map(schema: &String) -> Parts {
-    let mut parts = Parts{map: Vec::new()};
+    let mut parts = Parts{map: Vec::new(), x_dim: 0, y_dim: 0};
     let lines: Vec<&str> = schema.split('\n').collect();
     for line in lines {
         let mut v: Vec<char> = Vec::new();
-        for c in line.trim().chars() {
+        for (j, c) in line.trim().chars().enumerate() {
             v.push(c);
         }
         parts.map.push(v);
     }
+    parts.init();
     return parts;
 }
 
