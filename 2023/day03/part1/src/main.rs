d4b209d656bd56bda012e4ba1c684fa8faf64a77
giacomo <giacomo@amoroso.me>
2023-12-09 12:41:41 +0100

    Fix test version part one

--- 2023/day03/part1/src/main.rs
+++ 2023/day03/part1/src/main.rs
@@ -5,7 +5,7 @@ mod util;
 
 fn main() {
     // let puzzle_input = std::fs::read_to_string("assets/input.txt").expect("Should contain the puzzle input.");
-    let file = File::open("assets/input.txt").expect("Should contain the puzzle input.");
+    let file = File::open("assets/test_input.txt").expect("Should contain the puzzle input.");
     let reader = BufReader::new(file);
 
     let sum = util::gears_sum(reader);
--- 2023/day03/part1/src/util/gear_analyzer_02.rs
+++ 2023/day03/part1/src/util/gear_analyzer_02.rs
@@ -1,13 +1,40 @@
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
-struct PartNumber {
-    val: u32,
-    row: i32,
-    start: i32,
-    end: i32
+struct ValidPosition {
+    pub row: i32,
+    pub col: i32,
 }
 
+#[derive(Debug, Copy, Clone)]
+struct PartNumber {
+    val: u32,
+    pub row: i32,
+    pub start: i32,
+    pub end: i32
+}
+
+impl PartNumber {
+    fn custom_equals(&self, other: &Self) -> bool {
+        self.val == other.val
+    }
+}
+
+impl Eq for PartNumber {}
+
+impl PartialEq for PartNumber {
+    fn eq(&self, other: &Self) -> bool {
+        self.custom_equals(other)
+    }
+}
+
+impl Hash for PartNumber {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.val.hash(state);
+    }
+}
 
 type Schema = Vec<Vec<char>>;
 
@@ -20,10 +47,11 @@ pub fn gears_sum(reader: BufReader<File>) -> u32 {
     }
 
     let numbers = find_numbers(&schema);
+    let valid_part_numbers = mark_valid_part_numbers(&numbers, &schema);
 
-    println!("Numbers: {}", numbers.len());
+    let values: Vec<u32> = valid_part_numbers.iter().map(|v| v.val).collect();
  
-    return 0;
+    return values.iter().sum::<u32>();
 }
 
 fn find_numbers(schema: &Schema) -> Vec<PartNumber> {
@@ -31,10 +59,8 @@ fn find_numbers(schema: &Schema) -> Vec<PartNumber> {
     let mut parts: Vec<PartNumber> = Vec::new();
 
     for (row, line) in schema.iter().enumerate() {
-        
         let mut buf: Vec<char> = Vec::new();
         for (col, c) in line.iter().enumerate() {
-            
             if !c.is_digit(10) {
                 if !buf.is_empty() {
                     let number: String = buf.iter().collect();
@@ -45,9 +71,72 @@ fn find_numbers(schema: &Schema) -> Vec<PartNumber> {
             else if c.is_digit(10) {
                 buf.push(*c);
             }
-
         }
     }
 
     return parts;
+}
+
+fn mark_valid_part_numbers(part_numbers: &Vec<PartNumber>, schema: &Schema) -> HashSet<PartNumber> {
+    let mut valid_part_number:HashSet<PartNumber> = HashSet::new();
+
+    for part_number in part_numbers {
+        if is_valid_part_number(part_number, schema) {
+            valid_part_number.insert(part_number.clone());
+        }
+    }
+
+    return valid_part_number;
+}
+
+fn is_valid_part_number(part_number: &PartNumber, schema: &Schema) -> bool {
+ 
+    let mut valid_positions:Vec<ValidPosition>= Vec::new();
+    for col in part_number.start..=part_number.end {
+        valid_positions.push(ValidPosition { row: part_number.row - 1, col: col - 1});
+        valid_positions.push(ValidPosition { row: part_number.row - 1, col: col});
+        valid_positions.push(ValidPosition { row: part_number.row - 1, col: col + 1});
+        valid_positions.push(ValidPosition { row: part_number.row, col: col - 1});
+        valid_positions.push(ValidPosition { row: part_number.row, col: col + 1});
+        valid_positions.push(ValidPosition { row: part_number.row + 1, col: col - 1});
+        valid_positions.push(ValidPosition { row: part_number.row + 1, col: col});
+        valid_positions.push(ValidPosition { row: part_number.row + 1, col: col + 1});
+    }
+    
+    for pos in valid_positions {
+        let (mx_row, mx_col) = (schema.len() - 1, schema[0].len() - 1);
+        let row: i32 = if pos.row == -1 { 0 } else if pos.row > mx_row as i32 { mx_row as i32 } else { pos.row };
+        let col: i32 = if pos.col == -1 { 0 } else if pos.col > mx_col as i32 { mx_col as i32 } else { pos.col };
+
+        let schema_pos: char = schema[row as usize][col as usize];
+
+        // It's a symbol
+        if !schema_pos.is_digit(10) && schema_pos != '.' {
+            return true;
+        }
+
+    }
+
+    return false;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_calculate_correct_sum() {
+        let file = File::open("assets/test_input.txt").expect("Should contain the puzzle input.");
+        let reader = BufReader::new(file);
+
+        assert_eq!(gears_sum(reader), 4361);
+    }
+
+    #[test]
+    fn should_test_mod() {
+        let neg:i32 = -1;
+  
+        assert_eq!(neg.rem_euclid(10), 0);
+    }
 }
