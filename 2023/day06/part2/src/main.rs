eebcffdf85f19b9557fa4246cbc1343280813565
Giacomo Amoroso <giacomo@amoroso.me>
2024-01-08 21:43:09 +0100

    D6P2 Setup

--- 2023/day06/part2/src/main.rs
+++ 2023/day06/part2/src/main.rs
@@ -1,3 +1,12 @@
+mod utils;
+mod competition;
+
+use utils::parse_races;
+
+use crate::utils::error_margin;
+
 fn main() {
-    println!("Hello, world!");
+    let races = parse_races(&"../assets/races.txt");
+
+    println!("[SOLUTION]: {}", error_margin(&races));
 }
