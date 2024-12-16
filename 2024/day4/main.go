aefd15163033de6b15bad0e2e8c142c092620bf1
Giacomo Amoroso <giacomo@amoroso.me>
2024-12-16 22:21:41 +0100

    WIP DAY 4 part2

--- 2024/day4/main.go
+++ 2024/day4/main.go
@@ -18,6 +18,13 @@ var directions = [][2]int{
 	{-1, 1},  // Diagonal bottom-left to top-right
 }
 
+var crossDirections = [][2]int{
+	{1, 1},   // Diagonal top-left to bottom-right
+	{-1, -1}, // Diagonal bottom-right to top-left
+	{1, -1},  // Diagonal top-right to bottom-left
+	{-1, 1},  // Diagonal bottom-left to top-right
+}
+
 // Function to check if the position is within bounds
 func isInBounds(x, y, rows, cols int) bool {
 	return x >= 0 && x < rows && y >= 0 && y < cols
@@ -65,6 +72,26 @@ func reverseString(s string) string {
 	return string(runes)
 }
 
+func findWordAsXShape(grid [][]rune, word string) [][2]int {
+	reverseWord := reverseString(word)
+	rows, cols := len(grid), len(grid[0])
+	var occurrences [][2]int
+
+	for x := 0; x < rows; x++ {
+		for y := 0; y < cols; y++ {
+			// Check all directions from this starting point
+			for _, dir := range crossDirections {
+				dx, dy := dir[0], dir[1]
+				if search(grid, word, x, y, dx, dy) || search(grid, reverseWord, x, y, dx, dy) {
+					occurrences = append(occurrences, [2]int{x, y})
+				}
+			}
+		}
+	}
+
+	return occurrences
+}
+
 func readInput(filename string) string {
 	input, err := os.ReadFile(filename)
 	if err != err {
@@ -81,10 +108,26 @@ func Day1(grid [][]rune, word string) {
 
 	// Print occurrences
 	fmt.Println("Occurrences of XMAS (or SAMX):")
-	fmt.Printf("Total: %d\n", len(occurrences)/2)
+	fmt.Printf("Total: %d\n", len(occurrences)/2) // Dived by 2 because we have double counts
 	// for _, occ := range occurrences {
 	// 	fmt.Printf("Start at row %d, col %d\n", occ[0]+1, occ[1]+1)
 	// }
+}
+
+func Day2(grid [][]rune, word string) {
+	occurences := findWordAsXShape(grid, word)
+	fmt.Printf("Total: %d\n", len(occurences)/2)
+
+}
+
+func generateGrid(input string) [][]rune {
+	// Convert input to 2D rune slice
+	lines := strings.Split(input, "\n")
+	grid := make([][]rune, len(lines))
+	for i, line := range lines {
+		grid[i] = []rune(line)
+	}
+	return grid
 }
 
 func main() {
@@ -101,15 +144,35 @@ func main() {
 	// MXMXAXMASX`
 
 	input := strings.TrimSpace(readInput("input.txt"))
-
-	// Convert input to 2D rune slice
-	lines := strings.Split(input, "\n")
-	grid := make([][]rune, len(lines))
-	for i, line := range lines {
-		grid[i] = []rune(line)
-	}
+	grid := generateGrid(input)
 
 	// Word to find
 	Day1(grid, "XMAS")
 
-}S
+	input = `.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........`
+
+	// input = `..........
+	// S.M.......
+	// .A........
+	// S.M.......
+	// ..........`
+
+	// input = `..........
+	// S.S.......
+	// .A........
+	// M.M.......
+	// ..........`
+	grid = generateGrid(strings.TrimSpace(input))
+
+	Day2(grid, "MAS")
+
+}
